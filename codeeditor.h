1751f31f107587b3082308cd794628774fdb3ca8
jason <yishun1015@gmail.com>
2026-06-22 06:29:38 +0100

    improvements in functionalities and experience

--- codeeditor.h
+++ codeeditor.h
@@ -2,7 +2,68 @@
 #define CODEEDITOR_H
 
 #include <QPlainTextEdit>
+#include <QRegularExpression>
+#include <QString>
+#include <QSyntaxHighlighter>
+#include <QTextCharFormat>
+#include <QTextDocument>
+#include <QVector>
 #include <QWidget>
+
+class CodeHighlighter : public QSyntaxHighlighter
+{
+public:
+    explicit CodeHighlighter(QTextDocument *parent = nullptr);
+    void setLanguage(const QString &language);
+
+protected:
+    void highlightBlock(const QString &text) override;
+
+private:
+    struct HighlightingRule {
+        QRegularExpression pattern;
+        QTextCharFormat format;
+        int captureIndex = 0;
+    };
+
+    void initializeFormats();
+    void addRule(const QString &pattern, const QTextCharFormat &format, int captureIndex = 0);
+    void setupCStyle();
+    void setupPython();
+    void setupJava();
+    void setupJavaScript();
+    void setupTypeScript();
+    void setupRust();
+    void setupGo();
+    void setupShell();
+    void setupHtml();
+    void setupCss();
+    void setupPlainText();
+
+    void handleCStyleBlockComment(const QString &text);
+    void handlePythonTripleString(const QString &text);
+    void handleHtmlComment(const QString &text);
+
+    QString language;
+    QVector<HighlightingRule> rules;
+    bool inBlockComment = false;
+    bool inTripleString = false;
+    bool inHtmlComment = false;
+    QString tripleDelimiter;
+
+    QTextCharFormat keywordFormat;
+    QTextCharFormat stringFormat;
+    QTextCharFormat commentFormat;
+    QTextCharFormat numberFormat;
+    QTextCharFormat preprocessorFormat;
+    QTextCharFormat tagFormat;
+    QTextCharFormat attributeFormat;
+    QTextCharFormat cssPropertyFormat;
+    QTextCharFormat variableFormat;
+    QTextCharFormat functionFormat;
+    QTextCharFormat escapeFormat;
+    QTextCharFormat trailingSpaceFormat;
+};
 
 class CodeEditor : public QPlainTextEdit
 {
@@ -10,6 +71,8 @@ class CodeEditor : public QPlainTextEdit
     friend class LineNumberArea;
 public:
     CodeEditor(QWidget *parent = nullptr);
+    void setLanguageForFile(const QString &filePath);
+    void enableIndentationMarkers();
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
@@ -24,6 +87,7 @@ private:
     int lineNumberAreaWidth() const;
 
     QWidget *lineNumberArea;
+    CodeHighlighter *syntaxHighlighter;
 };
 
 class LineNumberArea : public QWidget
